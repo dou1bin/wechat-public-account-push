418270309d35be744fa5f5bd2bad226d7f6338bc
dou1bin <131668878+dou1bin@users.noreply.github.com>
2023-04-28 01:58:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,22 +42,22 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-04-15' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-04-15' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '6S4jeKPUni-ADf9XktckM7GdRq9dLW4O-_HIvZQ0qVw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wx0e0bdc924c396dc6',
     }
   ],
 
