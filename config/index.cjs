7405acff1265a8838b0d7ed0529d17cabf5fe6e7
dou1bin <131668878+dou1bin@users.noreply.github.com>
2023-04-28 02:05:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCSqh6swsjHRZsUyI_ZGroO0y2Lg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'SlY4wUetXGNG9puAk7_leHD7R3VlagxCR5V_p5CF-TU',
+      useTemplateId: 'R_SUHTANNgjUYGpysuSTBTnhI1tsGDQFFUguKayPeWs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '6S4jeKPUni-ADf9XktckM7GdRq9dLW4O-_HIvZQ0qVw',
+  CALLBACK_TEMPLATE_ID: 'R_SUHTANNgjUYGpysuSTBTnhI1tsGDQFFUguKayPeWs',
 
   CALLBACK_USERS: [
     {
